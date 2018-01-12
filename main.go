9651b8a4bb7d6b01d10a7920b4e7ff1919e4d8f1
William Goodall <wgoodall01@gmail.com>
2018-01-12 11:10:50 -0500

    CLI improvements

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"github.com/urfave/cli"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -11,9 +14,16 @@ func NewFileError(msg string) *cli.ExitError {
 	return cli.NewExitError(msg, 1)
 }
 
-var listCmd = cli.Command{
-	Name:  "list",
-	Usage: "List all articles in an archive",
+func prompt(prompt string) string {
+	in := bufio.NewReader(os.Stdin)
+	fmt.Printf("%-15s: ", prompt)
+	valRaw, _ := in.ReadString('\n')
+	return strings.TrimSpace(valRaw)
+}
+
+var findCmd = cli.Command{
+	Name:  "find",
+	Usage: "Find articles from the archive.",
 	Flags: []cli.Flag{
 		cli.StringFlag{
 			Name:  "file, f",
@@ -28,14 +38,44 @@ var listCmd = cli.Command{
 			return NewFileError("Could not open wiki")
 		}
 
-		// Article visitor
+		// Articles to get
+		articleNames := make([]string, len(c.Args()))
+		for i, arg := range c.Args() {
+			articleNames[i] = NormalizeArticleTitle(arg)
+		}
+
 		callback := func(a *Article) error {
-			fmt.Printf("%10d%3d%30s%10s\n", a.Id, a.Namespace, a.Title, a.RevisionAuthor)
-			links := ParseLinks(a.Text)
-			for _, link := range links {
-				fmt.Println(link)
+			for i, name := range articleNames {
+				if NormalizeArticleTitle(a.Title) == name {
+
+					fmt.Printf("%20s : %-50s\n", "Article Title", a.Title)
+					fmt.Printf("%20s : %-50s\n", "Redirect", a.Redirect.Title)
+					fmt.Printf("%20s : %-50d\n", "ID", a.Id)
+					fmt.Printf("%20s : %-50d\n", "Namespace", a.Namespace)
+					fmt.Printf("%20s : %-50s\n", "Timestamp", a.RevisionTimestamp)
+					fmt.Printf("%20s : %-50s\n", "Author", a.RevisionAuthor)
+
+					fmt.Printf("Text ::\n\n")
+					fmt.Println(a.Text)
+					fmt.Println("\n::\n\n")
+
+					fmt.Printf("Links ::\n")
+					for _, link := range ParseLinks(a.Text) {
+						fmt.Print(link + ", ")
+					}
+					fmt.Println("::\n")
+
+					articleNames[i] = ""
+
+					if i == len(articleNames)-1 {
+						fmt.Println("Done searching")
+						return errors.New("Done searching.")
+					} else {
+						return nil
+					}
+
+				}
 			}
-			fmt.Println()
 			return nil
 		}
 
@@ -56,7 +96,7 @@ var startCmd = cli.Command{
 	},
 	Action: func(c *cli.Context) error {
 		// Open the archive
-		fmt.Print("Opening archive... ")
+		fmt.Print("Opening archive...  ")
 		tOpen := time.Now()
 		path := c.String("file")
 		archiveFile, fileErr := os.Open(path)
@@ -64,7 +104,7 @@ var startCmd = cli.Command{
 			return NewFileError("Could not open wiki")
 		}
 		dOpen := time.Since(tOpen).Seconds()
-		fmt.Printf("[done in %.2fs]\n", dOpen)
+		fmt.Printf("[done in %4.2fs]\n", dOpen)
 
 		// Load all the articles.
 		fmt.Print("Loading articles... ")
@@ -72,34 +112,53 @@ var startCmd = cli.Command{
 		ind := NewIndex()
 		LoadWikiToIndex(archiveFile, ind)
 		dLoad := time.Since(tLoad).Seconds()
-		fmt.Printf("[done in %.2fs]\n", dLoad)
+		fmt.Printf("[done in %4.2fs]\n", dLoad)
 
 		// Index all the articles.
-		fmt.Print("Making index... ")
+		fmt.Print("Making index...     ")
 		tBuild := time.Now()
 		ind.Build()
 		dBuild := time.Since(tBuild).Seconds()
-		fmt.Printf("[done in %.2fs]\n", dBuild)
+		fmt.Printf("[done in %4.2fs]\n", dBuild)
 
 		// Find a path.
-		running := true
-		for running {
+	InputLoop:
+		for true {
 			fmt.Print("\n\n")
-			var articleA string
-			var articleB string
+			names := [2]string{}
+			items := [2]*IndexItem{}
 
-			fmt.Print("First Article  : ")
-			fmt.Scanln(&articleA)
+			names[0] = prompt("First Article")
+			names[1] = prompt("Second Article")
 
-			fmt.Print("Second Article : ")
-			fmt.Scanln(&articleB)
-
-			fmt.Printf("Searching for path from %s -> %s...\n\n", articleA, articleB)
-
-			paths := ind.FindPath(ind.Get(articleA), ind.Get(articleB), 4)
-			for _, item := range paths[0] {
-				fmt.Print(item.Title + ",")
+			for i, _ := range names {
+				items[i] = ind.Get(names[i])
+				if items[i] == nil {
+					fmt.Printf("Error: Can't find article '%s'", names[i])
+					continue InputLoop
+				}
 			}
+
+			tSearch := time.Now()
+			fmt.Printf("\nSearching for path... ")
+			nSteps := 5
+			paths := ind.FindPath(items[0], items[1], nSteps)
+			dSearch := time.Since(tSearch).Seconds()
+			fmt.Printf("[done in %4.2f]\n", dSearch)
+
+			if len(paths) == 0 {
+				fmt.Printf("No paths found in %d steps.", nSteps)
+			} else {
+				fmt.Printf("Path: ")
+				for i, item := range paths[0] {
+					if i != 0 {
+						fmt.Printf(" -> ")
+					}
+					fmt.Print(item.Title)
+				}
+				fmt.Println()
+			}
+
 			fmt.Println()
 			ind.Reset()
 		}
@@ -114,7 +173,7 @@ func main() {
 	app.HelpName = app.Name
 	app.Usage = "Find a path of links between two wiki pages."
 
-	app.Commands = []cli.Command{listCmd, startCmd}
+	app.Commands = []cli.Command{findCmd, startCmd}
 
 	app.Run(os.Args)
 }
