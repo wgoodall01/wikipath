faedc2592cc515d368a36de622cd45139de01bb2
William Goodall <wgoodall01@gmail.com>
2018-12-27 22:14:00 -0500

    download.sh: update URLs

--- cli/wikis/download.sh
+++ cli/wikis/download.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+ID="20181220" # https://dumps.wikimedia.org/enwiki/{ID}/
+
+# cd to script location
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 aria2c -c -x16 -s16 -i - <<EOF
-https://dumps.wikimedia.org/simplewiki/20180101/simplewiki-20180101-pages-articles-multistream.xml.bz2
+https://dumps.wikimedia.org/simplewiki/${ID}/simplewiki-${ID}-pages-articles-multistream.xml.bz2
 	out=simple.xml.bz2
-	checksum=sha-1=e1dbcfd1fdf421572f68638ab5230e675295e215
 
-https://dumps.wikimedia.org/simplewiki/20180101/simplewiki-20180101-pages-articles-multistream-index.txt.bz2
+https://dumps.wikimedia.org/simplewiki/${ID}/simplewiki-${ID}-pages-articles-multistream-index.txt.bz2
 	out=simple-index.txt.bz2
-	checksum=sha-1=df180b0934d55b32859ac3216833dcc8f0c82bf9
 
-https://dumps.wikimedia.org/enwiki/20180101/enwiki-20180101-pages-articles-multistream.xml.bz2
+https://dumps.wikimedia.org/enwiki/${ID}/enwiki-${ID}-pages-articles-multistream.xml.bz2
 	out=complete.xml.bz2
-	checksum=sha-1=0a4bee239be61a8ef77c2e72d9b4546863c57264
 
-https://dumps.wikimedia.org/enwiki/20180101/enwiki-20180101-pages-articles-multistream-index.txt.bz2
+https://dumps.wikimedia.org/enwiki/${ID}/enwiki-${ID}-pages-articles-multistream-index.txt.bz2
 	out=complete-index.txt.bz2
-	checksum=sha-1=8de15c617180d9cb8798d8bb60b2090894ef6c42
 EOF
+
+# maybe checksums in future:
+#	checksum=sha-1=8de15c617180d9cb8798d8bb60b2090894ef6c42
